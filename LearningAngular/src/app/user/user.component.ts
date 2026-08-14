c2efb5cd3a6c0c1837bc70b3698757aa1c14fb74
Gian Carlos Juarez Velasquez <gjuarez@defensoria.gob.pe>
2026-08-14 11:09:47 -0500

    using output

--- LearningAngular/src/app/user/user.component.ts
+++ LearningAngular/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 //import {Component, Input, input, computed} from '@angular/core';
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input, output} from '@angular/core';
 
 
 @Component({
@@ -20,7 +20,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select= new EventEmitter();
+  select = output<string>()
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
